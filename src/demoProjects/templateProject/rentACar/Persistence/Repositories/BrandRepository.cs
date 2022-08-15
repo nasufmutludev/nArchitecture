03cc478b75b755774a3ee58afdc555cd8f944245
Nasuf MUTLU <nasufmutlu@hotmail.com>
2022-08-16 00:32:25 +0300

    Add project files.

---
+++ src/demoProjects/templateProject/rentACar/Persistence/Repositories/BrandRepository.cs
@@ -0,0 +1,19 @@
+﻿using Core.Persistence.Repositories;
+using Persistence.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+using Application.Services.Repositories;
+
+namespace Persistence.Repositories
+{
+    public class BrandRepository: EfRepositoryBase<Brand, BaseDbContext>, IBrandRepository
+    {
+        public BrandRepository(BaseDbContext context) : base(context)
+        {
+        }
+    }
+}
