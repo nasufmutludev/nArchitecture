0a3c66a140bee7b8757e65f15bd9de15206781ea
Nasuf MUTLU <nasufmutlu@hotmail.com>
2022-09-11 16:41:23 +0300

    .

---
+++ src/demoProjects/templateProject/rentACar/Application/Features/Brands/Commands/CreateBrand/CreateBrandCommentValidator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentValidation;
+
+namespace Application.Features.Brands.Commands.CreateBrand
+{
+    public class CreateBrandCommentValidator:AbstractValidator<CreateBrandEntityCommand>
+    {
+        public CreateBrandCommentValidator()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Name).MinimumLength(2);
+        }
+    }
+}
